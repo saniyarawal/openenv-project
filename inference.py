cad1e955b9505715c636ddd024d767ba460ff9ad
saniyarawal <saniyarawalrawal0@gmail.com>
2026-03-31 23:32:27 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import random
 
 app = FastAPI()
 
+# Request model for step
+class Action(BaseModel):
+    action: int = 0
+
 @app.post("/reset")
 def reset():
-    return {"state": 0}
+    return {
+        "observation": 0,
+        "reward": 0,
+        "done": False,
+        "info": {}
+    }
 
 @app.post("/step")
-def step(action: int = 0):
-    reward = random.randint(0, 1)
-    done = random.choice([True, False])
-    return {"state": random.randint(0, 10), "reward": reward, "done": done}
+def step(action: Action):
+    return {
+        "observation": random.randint(0, 10),
+        "reward": random.randint(0, 1),
+        "done": random.choice([True, False]),
+        "info": {}
+    }
